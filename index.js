555b7f1cea0a1545f846423695bfbce5f9144025
Paras Anand <a_paras.anand@airtel.com>
2020-04-27 14:35:41 +0530

    Wrote the remove object duplicate function

--- index.js
+++ index.js
@@ -1,91 +1,112 @@
 (function() {
   let likesArray = [];
-  const LOOP_COUNT = 20;
+  const LOOP_COUNT = 10;
+  const WEBSITE = "https://www.instagram.com";
 
   function getFormattedDate() {
-    var date = new Date();
+      var date = new Date();
 
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
+      var month = date.getMonth() + 1;
+      var day = date.getDate();
+      var hour = date.getHours();
+      var min = date.getMinutes();
+      var sec = date.getSeconds();
 
-    month = (month < 10 ? "0" : "") + month;
-    day = (day < 10 ? "0" : "") + day;
-    hour = (hour < 10 ? "0" : "") + hour;
-    min = (min < 10 ? "0" : "") + min;
-    sec = (sec < 10 ? "0" : "") + sec;
+      month = (month < 10 ? "0" : "") + month;
+      day = (day < 10 ? "0" : "") + day;
+      hour = (hour < 10 ? "0" : "") + hour;
+      min = (min < 10 ? "0" : "") + min;
+      sec = (sec < 10 ? "0" : "") + sec;
 
-    //     var str = date.getFullYear() + "-" + month + "-" + day + "_" +  hour + ":" + min + ":" + sec;
+      //     var str = date.getFullYear() + "-" + month + "-" + day + "_" +  hour + ":" + min + ":" + sec;
 
-    var str = "Time - " + hour + ":" + min + ":" + sec;
+      var str = "Time - " + hour + ":" + min + ":" + sec;
 
-    /* alert(str); */
-
-    return str;
+      return str;
   }
 
-  function removeDuplicates(array) {
-    return array.filter((a, b) => array.indexOf(a) === b);
+  function removeDuplicates(arr) {
+      const uniqueArray = arr.filter((thing,index)=>{
+          const _thing = JSON.stringify(thing);
+          return index === arr.findIndex(obj=>{
+              return JSON.stringify(obj) === _thing;
+          }
+          );
+      }
+      );
+
+      return uniqueArray;
+  }
+
+  function dynamicSort(property) {
+      var sortOrder = 1;
+      if (property[0] === "-") {
+          sortOrder = -1;
+          property = property.substr(1);
+      }
+      return function(a, b) {
+          /* next line works with strings and numbers,
+     * and you may want to customize it to your needs
+     */
+          var result = b[property] < a[property] ? -1 : b[property] > a[property] ? 1 : 0;
+          return result * sortOrder;
+      }
+      ;
   }
   function grabLikes() {
-    function simulateMouseover() {
-      let i = 0;
+      function simulateMouseover() {
+          let i = 0;
 
-      const rowlength = document.querySelectorAll(
-        "#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div div.Nnq7C.weEfm"
-      ).length;
+          const rowlength = document.querySelectorAll("#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div div.Nnq7C.weEfm").length;
 
-      for (i = 0; i < rowlength * 3; i++) {
-        var event = new MouseEvent("mouseover", {
-          "view": window,
-          "bubbles": true,
-          "cancelable": true
-        });
+          for (i = 0; i < rowlength * 3; i++) {
+              var event = new MouseEvent("mouseover",{
+                  "view": window,
+                  "bubbles": true,
+                  "cancelable": true
+              });
 
-        const rowNumber = parseInt(i / 3) + 1;
-        const columnNumber = (i % 3) + 1;
+              const rowNumber = parseInt(i / 3) + 1;
+              const columnNumber = (i % 3) + 1;
 
-        const cssSelector =
-          "#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div > div:nth-child(" +
-          rowNumber +
-          ") > div:nth-child(" +
-          columnNumber +
-          ") > a > div.eLAPa";
-        var locaterHandle = document.querySelector(cssSelector);
+              const cssSelector = "#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div > div:nth-child(" + rowNumber + ") > div:nth-child(" + columnNumber + ") > a > div.eLAPa";
+              var locaterHandle = document.querySelector(cssSelector);
 
-        var canceled = !locaterHandle.dispatchEvent(event);
+              var canceled = !locaterHandle.dispatchEvent(event);
+          }
       }
-    }
 
-    function callback() {
-      var divs = document.querySelectorAll(".qn-0x li:first-child");
+      function getValuesFromCards() {
+          var anchors = document.querySelectorAll("main > div > div._2z6nI > article > div:nth-child(1) > div div div > a");
 
-      for (i = 0; i < divs.length; i++) {
-        let value = divs[i].innerText;
+          for (i = 0; i < anchors.length; i++) {
+              let hrefValue = anchors[i].getAttribute("href");
+              let likeCount = anchors[i].querySelector(".qn-0x li:first-child");
 
-        // remove whitespace
-        value = value.replace(/ /g, "");
-        value = value.replace(/,/g, "");
+              let value = likeCount.innerText;
 
-        if (value.indexOf("k") > 0) {
-          value = value.replace(/k/g, "");
-          value = parseFloat(value) * 1000;
-        }
+              // remove whitespace
+              value = value.replace(/ /g, "");
+              value = value.replace(/,/g, "");
 
-        //         console.log(value)
-        likesArray.push(parseInt(value));
+              if (value.indexOf("k") > 0) {
+                  value = value.replace(/k/g, "");
+                  value = parseFloat(value) * 1000;
+              }
+
+              //         console.log(value)
+              likesArray.push({
+                  href: WEBSITE + hrefValue,
+                  likes: parseInt(value)
+              });
+          }
       }
-    }
 
-    simulateMouseover();
+      simulateMouseover();
 
-    callback();
+      getValuesFromCards();
 
-    likesArray.sort(function(a, b) {
-      return b - a;
-    });
+      likesArray.sort(dynamicSort("likes"));
   }
 
   // console.clear();
@@ -93,22 +114,25 @@
   console.log(getFormattedDate());
 
   async function scrollToBottom() {
-    return new Promise((resolve, reject) => {
-      window.scrollTo(0, document.body.scrollHeight);
-      setTimeout(() => resolve(), 2000);
-    });
+      return new Promise((resolve,reject)=>{
+          window.scrollTo(0, document.body.scrollHeight);
+          setTimeout(()=>resolve(), 2000);
+      }
+      );
   }
 
   async function main() {
-    for (let j = 0; j < LOOP_COUNT; j++) {
-      grabLikes();
-      await scrollToBottom();
-    }
+      for (let j = 0; j < LOOP_COUNT; j++) {
+          grabLikes();
+          await scrollToBottom();
+      }
   }
 
   (async function() {
-    console.clear();
-    await main();
-    console.log(JSON.stringify(removeDuplicates(likesArray), null, "\t"));
-  })();
-})();
+      console.clear();
+      await main();
+      console.log(JSON.stringify(removeDuplicates(likesArray), null, "\t"));
+  }
+  )();
+}
+)();
