d3debb99859c14153d12cab412a5bf7ac39242f1
qtcoder999 <30284500+qtcoder999@users.noreply.github.com>
2021-02-26 13:36:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,38 +3,43 @@
     const WEBSITE = "https://www.instagram.com";
     const watchedElement = document.querySelector("article.ySN3v");
     const DEBOUNCED_RATE = 4000;
+    const DEBOUNCED_EXCEPTION_RATE = 100000;
     let disconnect = false;
 
     function debounce(n, t, u) {
         var e;
         return function() {
-            var i = this,
-                o = arguments;
-            clearTimeout(e), e = setTimeout(function() {
-                e = null, u || n.apply(i, o)
-            }, t), u && !e && n.apply(i, o)
+            var i = this
+              , o = arguments;
+            clearTimeout(e),
+            e = setTimeout(function() {
+                e = null,
+                u || n.apply(i, o)
+            }, t),
+            u && !e && n.apply(i, o)
         }
     }
 
     const observeDOM = (function() {
-        var MutationObserver =
-            window.MutationObserver || window.WebKitMutationObserver;
+        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
         return function(obj, callback) {
-            if (!obj || !obj.nodeType === 1) return;
+            if (!obj || !obj.nodeType === 1)
+                return;
             // validation
-           
+
             if (MutationObserver) {
                 // define a new observer
                 var obs = new MutationObserver(function(mutations, observer) {
-                   
-                    if (!disconnect) {   
+
+                    if (!disconnect) {
                         callback(mutations);
                     } else {
                         observer.disconnect();
                     }
 
-                });
+                }
+                );
 
                 obs.observe(obj, {
                     childList: true,
@@ -48,32 +53,39 @@
                 obj.removeEventListener("DOMNodeInserted", callback, false);
                 obj.removeEventListener("DOMNodeRemoved", callback, false);
             }
-        };
-    })();
+        }
+        ;
+    }
+    )();
 
     function getFormattedDate() {
-        var e = new Date,
-            t = e.getMonth() + 1,
-            a = e.getDate(),
-            g = e.getHours(),
-            n = e.getMinutes(),
-            r = e.getSeconds();
-        return g = (g < 10 ? "0" : "") + g, n = (n < 10 ? "0" : "") + n, r = (r < 10 ? "0" : "") + r, "Timestamp - " + (a = (a < 10 ? "0" : "") + a) + "-" + (t = (t < 10 ? "0" : "") + t) + "-" + e.getFullYear() + "_" + g + ":" + n + ":" + r
+        var e = new Date
+          , t = e.getMonth() + 1
+          , a = e.getDate()
+          , g = e.getHours()
+          , n = e.getMinutes()
+          , r = e.getSeconds();
+        return g = (g < 10 ? "0" : "") + g,
+        n = (n < 10 ? "0" : "") + n,
+        r = (r < 10 ? "0" : "") + r,
+        "Timestamp - " + (a = (a < 10 ? "0" : "") + a) + "-" + (t = (t < 10 ? "0" : "") + t) + "-" + e.getFullYear() + "_" + g + ":" + n + ":" + r
     }
 
     function removeDuplicates(n) {
-        return n.filter((i, r) => {
+        return n.filter((i,r)=>{
             const t = JSON.stringify(i);
-            return r === n.findIndex(n => JSON.stringify(n) === t)
-        })
+            return r === n.findIndex(n=>JSON.stringify(n) === t)
+        }
+        )
     }
 
     function dynamicSort(n) {
         var r = 1;
-        return "-" === n[0] && (r = -1, n = n.substr(1)),
-            function(t, u) {
-                return (u[n] < t[n] ? -1 : u[n] > t[n] ? 1 : 0) * r
-            }
+        return "-" === n[0] && (r = -1,
+        n = n.substr(1)),
+        function(t, u) {
+            return (u[n] < t[n] ? -1 : u[n] > t[n] ? 1 : 0) * r
+        }
     }
 
     function convertToNumber(value) {
@@ -91,16 +103,20 @@
         return value;
     }
 
+    function stopExecutionOnTooMuchDelay(){
+        throw new Error('Request Timeout');
+    }
+
+    const debouncedStopExecution = debounce(stopExecutionOnTooMuchDelay, DEBOUNCED_EXCEPTION_RATE);
+
     async function grabLikes() {
         function simulateMouseover() {
             let i = 0;
 
-            const rowlength = document.querySelectorAll(
-                "#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div div.Nnq7C.weEfm"
-            ).length;
+            const rowlength = document.querySelectorAll("#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div div.Nnq7C.weEfm").length;
 
             for (i = 0; i < rowlength * 3; i++) {
-                var event = new MouseEvent("mouseover", {
+                var event = new MouseEvent("mouseover",{
                     "view": window,
                     "bubbles": true,
                     "cancelable": true
@@ -109,36 +125,41 @@
                 const rowNumber = parseInt(i / 3) + 1;
                 const columnNumber = (i % 3) + 1;
 
-                const cssSelector =
-                    "#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div > div:nth-child(" +
-                    rowNumber +
-                    ") > div:nth-child(" +
-                    columnNumber +
-                    ") > a > div.eLAPa";
+                const cssSelector = "#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div > div:nth-child(" + rowNumber + ") > div:nth-child(" + columnNumber + ") > a > div.eLAPa";
                 var locaterHandle = document.querySelector(cssSelector);
-                if (locaterHandle) var canceled = !locaterHandle.dispatchEvent(event);
+                if (locaterHandle)
+                    var canceled = !locaterHandle.dispatchEvent(event);
             }
         }
 
         function getValuesFromCards() {
-            var anchors = document.querySelectorAll(
-                "main > div > div._2z6nI > article > div:nth-child(1) > div div div > a"
-            );
+            var anchors = document.querySelectorAll("main > div > div._2z6nI > article > div:nth-child(1) > div div div > a");
 
             for (i = 0; i < anchors.length; i++) {
                 let hrefValue = anchors[i].getAttribute("href");
                 let likeCount = anchors[i].querySelector(".qn-0x li:first-child");
                 let commentCount = anchors[i].querySelector(".qn-0x li:nth-child(2)");
 
-                likeCount = likeCount.innerText;
+                if (likeCount != null && likeCount != undefined) {
 
-                likeCount = convertToNumber(likeCount);
+                    likeCount = likeCount.innerText;
+                    likeCount = convertToNumber(likeCount);
 
-                commentCount = commentCount.innerText;
+                } else {
+                    likeCount = null;
+                }
 
-                commentCount = convertToNumber(commentCount);
+                if (commentCount != null && commentCount != undefined) {
 
-                //         console.log(value)
+                    commentCount = commentCount.innerText
+                    commentCount = convertToNumber(commentCount);
+
+                } else {
+
+                    commentCount = null;
+
+                }
+
                 likesArray.push({
                     href: WEBSITE + hrefValue,
                     likeCount: parseInt(likeCount),
@@ -146,7 +167,7 @@
                 });
             }
         }
-        return new Promise(resolve => {
+        return new Promise(resolve=>{
             simulateMouseover();
 
             getValuesFromCards();
@@ -154,29 +175,36 @@
             likesArray.sort(dynamicSort("likeCount"));
 
             resolve();
-        });
+        }
+        );
     }
 
     // console.clear();
 
     async function scrollToBottom() {
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve,reject)=>{
             window.scrollTo(0, document.body.scrollHeight);
-            setTimeout(() => resolve(), 2000);
-        });
+            setTimeout(()=>resolve(), 2000);
+        }
+        );
     }
 
+    debouncedStopExecution();
+
     observeDOM(watchedElement, async function(m) {
+        debouncedStopExecution();
         await main();
     });
 
-    const printOutput = () => {
+    const printOutput = ()=>{
         const output = removeDuplicates(likesArray);
         console.log(JSON.stringify(output, null, "\t"));
         disconnect = true;
-    };
+    }
+    ;
 
     const debouncedPrintOutput = debounce(printOutput, DEBOUNCED_RATE);
+
 
     async function main() {
         debouncedPrintOutput();
@@ -193,8 +221,11 @@
             await main();
         } catch (e) {
             console.log(e);
+            printOutput();
         } finally {
             debouncedPrintOutput();
         }
-    })();
-})();
+    }
+    )();
+}
+)();
